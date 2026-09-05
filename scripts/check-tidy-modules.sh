5a66ed643cbfb9d923ae341af91bce53d8e89efe
Chris Miles <chris.miles.e@gmail.com>
2026-09-05 02:13:55 +0000

    fix: make security evidence reproducible

--- scripts/check-tidy-modules.sh
+++ scripts/check-tidy-modules.sh
@@ -9,7 +9,6 @@ export GOMODCACHE="${GOMODCACHE:-/tmp/gomodcache}"
 
 bash scripts/check-published-modules.sh
 
-mapfile -t published_modules < scripts/published-modules.txt
 mapfile -t workspace_modules < <(
   GOWORK=off go work edit -json go.work |
     awk -F'"' '/"DiskPath":/ { path=$4; sub(/^\.[/]/, "", path); if (path == "") path="."; print path }'
@@ -27,21 +26,6 @@ for index in "${!workspace_modules[@]}"; do
   if [[ -f "$dir/go.sum" ]]; then
     cp "$dir/go.sum" "$check_sum"
   fi
-
-  current_path="$(awk '$1 == "module" { print $2; exit }' "$dir/go.mod")"
-  for published_dir in "${published_modules[@]}"; do
-    if [[ "$published_dir" == "." ]]; then
-      sibling_path="github.com/goforj/events"
-      sibling_dir="$root"
-    else
-      sibling_path="github.com/goforj/events/$published_dir"
-      sibling_dir="$root/$published_dir"
-    fi
-
-    if [[ "$sibling_path" != "$current_path" ]]; then
-      go mod edit -modfile="$check_mod" -replace="$sibling_path=$sibling_dir"
-    fi
-  done
 
   echo "tidy: $dir"
   if ! (cd "$dir" && GOWORK=off go mod tidy -modfile="$check_mod" -diff); then
